0c38ab2c202be0f04741ed87f3bd18f5dbf6816e
Sergey Blintsov <sergey.blintsov@drivepixels.ru>
2019-09-20 22:19:59 +0300

    Task 2 completed

--- modules/task_2/blintsov_s_conj_gradient_task2_omp/main.cpp
+++ modules/task_2/blintsov_s_conj_gradient_task2_omp/main.cpp
@@ -4,7 +4,7 @@
 #include <iostream>
 #include "omp.h"
 
-#define DEM 100
+#define DEM 400
 #define EPS 0.001
 
 float A[DEM][DEM];
@@ -62,9 +62,68 @@ float solve(float A[DEM][DEM], float F[DEM]) {
             Zk[i] = Rk[i] + beta * Zk[i];
     } while (Spr1 / mf > EPS * EPS && iterationCounter < MAX_ITERATIONS);
 
-    std::cout << "Ответ:"<< std::endl;
-    for (i = 0; i < DEM; i++)
-        std::cout << "(" << Xk[i] << ")" << std::endl;
+//    std::cout << "Ответ:"<< std::endl;
+//    for (i = 0; i < DEM; i++)
+//        std::cout << "(" << Xk[i] << ")" << std::endl;
+
+    return 0;
+}
+
+
+float omp_solve(float A[DEM][DEM], float F[DEM]) {
+    int i, j, kl = 1;
+    int MAX_ITERATIONS = 32767;
+
+    for (mf = 0, i = 0; i < DEM; i++) {
+        mf += F[i] * F[i];
+    }
+
+    for (i = 0; i < DEM; i++) {
+        Xk[i] = 0.2;
+    }
+
+    #pragma omp parallel for
+    for (i = 0; i < DEM; i++) {
+        for (Sz[i]=0, j = 0; j < DEM; j++)
+            Sz[i] += A[i][j] * Xk[j];
+        Rk[i] = F[i] - Sz[i];
+        Zk[i] = Rk[i];
+    }
+
+    int iterationCounter = 0;
+
+    do {
+        iterationCounter++;
+        Spz = 0;
+        Spr = 0;
+
+        #pragma omp parallel for
+        for (i = 0; i < DEM; i++) {
+            for (Sz[i] = 0, j = 0; j < DEM; j++) {
+                Sz[i] += A[i][j] * Zk[j];
+            }
+            Spz += Sz[i] * Zk[i];
+            Spr += Rk[i] * Rk[i];
+        }
+        alpha = Spr / Spz;
+
+        Spr1 = 0;
+        for (i = 0; i < DEM; i++) {
+            Xk[i] += alpha * Zk[i];
+            Rk[i] -= alpha * Sz[i];
+            Spr1 += Rk[i] * Rk[i];
+        }
+        kl++;
+
+        beta = Spr1 / Spr;
+
+        for (i = 0; i < DEM; i++)
+            Zk[i] = Rk[i] + beta * Zk[i];
+    } while (Spr1 / mf > EPS * EPS && iterationCounter < MAX_ITERATIONS);
+
+//    std::cout << "Ответ:"<< std::endl;
+//    for (i = 0; i < DEM; i++)
+//        std::cout << "(" << Xk[i] << ")" << std::endl;
 
     return 0;
 }
@@ -73,23 +132,23 @@ float solve(float A[DEM][DEM], float F[DEM]) {
 int main() {
     auto seed = static_cast<unsigned int>(time(0));
 
-    std::cout << "A:" << std::endl;
+//    std::cout << "A:" << std::endl;
     for (int i = 0; i < DEM; i++) {
-        std::cout << "( ";
+//        std::cout << "( ";
         for (int j = 0; j < DEM; j++) {
             A[i][i] = rand_r(&seed) % 100;
             if ((i != j) && (i < j)) {
                 A[i][j] = rand_r(&seed) % 100;
             }
             A[j][i] = A[i][j];
-            std::cout << A[j][i] << " ";
+//            std::cout << A[j][i] << " ";
         }
-        std::cout << ")" << std::endl;
+//        std::cout << ")" << std::endl;
     }
-    std::cout << "b:" << std::endl;
+//    std::cout << "b:" << std::endl;
     for (int j = 0; j < DEM; j++) {
         F[j] = rand_r(&seed) % 100;
-        std::cout << "( " << F[j] << " )" << std::endl;
+//        std::cout << "( " << F[j] << " )" << std::endl;
     }
 // Задаем вручную:
 //    A[0][0] = 3;
@@ -105,10 +164,16 @@ int main() {
 //    F[0] = 1;
 //    F[1] = 5;
 //    F[2] = 9;
-
+//
     clock_t time = clock();
     solve(A, F);
     time = clock() - time;
-    std::cout << "Время выполнения = " << static_cast<double>(time) << "мс" << std::endl;
+    std::cout << "Время выполнения последовательной версии = " << static_cast<double>(time) << "мс" << std::endl;
+
+    clock_t omp_time = clock();
+    omp_solve(A, F);
+    omp_time = clock() - omp_time;
+    std::cout << "Время выполнения OMP версии = " << static_cast<double>(omp_time) << "мс" << std::endl;
+
     return 0;
 }
